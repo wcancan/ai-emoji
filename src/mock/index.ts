c9944193000025aaec714563b708435d90df95cb
sy <suiyicc@outlook.com>
2025-01-03 11:03:26 +0800

    css

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -11,8 +11,18 @@ export default [
                 "success": true,
                 "message": "请求成功",
                 "data": [{
-                  templateId: "m6486767"
-                }]
+                  templateId: "m6486767",
+                  groupName: "表情名称",
+                  coverUrl: "https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800"
+                },{
+                    templateId: "m6486767",
+                    groupName: "表情名称",
+                    coverUrl: "https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800"
+                  },{
+                    templateId: "m6486767",
+                    groupName: "表情名称",
+                    coverUrl: "https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800"
+                  }]
             };
         },
     },
