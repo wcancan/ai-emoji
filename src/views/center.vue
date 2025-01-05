531165de334f12f6cb6ea8c967445d8ecdbae849
sy <suiyicc@outlook.com>
2025-01-05 17:33:41 +0800

    个人中心表情包详情

--- src/views/center.vue
+++ src/views/center.vue
@@ -3,48 +3,43 @@
     <div class="top">
       <div class="top-con">
         <div class="banner">
-            <img :src="previewEmojiData.avatar" alt="">
+          <img :src="previewEmojiData.coverLocalUrl || previewEmojiData.webpUrl" alt="">
         </div>
-        <div class="title txt-c">{{previewEmojiData.name}}</div>
-        
-        </div>
-        <div class="tips txt-c"><i></i>长按或截图保存<i class="tips-r"></i></div>
+        <div class="title txt-c">{{previewEmojiData.templateName || previewEmojiData.fileName}}</div>
+
+      </div>
+      <div class="tips txt-c"><i></i>长按或截图保存<i class="tips-r"></i></div>
     </div>
     <div class="list">
-        <div
-            class="item-container"
-            v-for="(item, index) in emojiList"
-            :key="index"
-        >
-            <div class="item-box" @click="handlePreview(item)">
-                <div class="avatar">
-                    <img :class="{'filter': item.status != 0 }" :src="item.avatar" />
-                    <div class="opa flex-align-end flex-center f12 col-white " v-if="item.status != 0">
-                       <p class="re-btn"><span>重新生成</span></p>
-                        <div class="flex flex-center re-text" v-if="item.status == 1">
-                            <IconFont :name="iconLoading" class="nut-icon-am-rotate nut-icon-am-infinite"></IconFont>
-                            <span>生成中</span>
-                        </div>
-                        <div class="flex flex-center re-text" v-if="item.status == 2" @click="handleGenerate">
-                          <IconFont :name="iconError" ></IconFont>
-                          <span>生成失败</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="title"><span>{{item.name}}</span></div>
+      <div class="item-container " :class="{'selected': previewEmojiData.id == item.id}"
+        v-for="(item, index) in emojiList" :key="index">
+        <div class="item-box" @click="handlePreview(item)">
+          <div class="avatar">
+            <img :class="{'filter': item.status != 2 }" :src="item.webpUrl" />
+            <div class="opa flex-align-end flex-center f12 col-white " v-if="item.status != 2">
+              <p class="re-btn"><span>重新生成</span></p>
+              <div class="flex flex-center re-text" v-if="item.status == 1">
+                <IconFont :name="iconLoading" class="nut-icon-am-rotate nut-icon-am-infinite"></IconFont>
+                <span>{{item.statusDesc}}</span>
+              </div>
+              <div class="flex flex-center re-text" v-if="item.status == 3" @click="handleGenerate(item)">
+                <IconFont :name="iconError"></IconFont>
+                <span>{{item.statusDesc}}</span>
+              </div>
             </div>
+          </div>
+          <div class="title"><span>{{item.fileName}}</span></div>
         </div>
+      </div>
     </div>
 
-     <nut-dialog
-      v-model:visible="isDialogVisible"
-    >
-    <i class="close-pop"  @click="isDialogVisible= false"></i>
-      <span>确认是否重新生成？</span>
+    <nut-dialog v-model:visible="isDialogVisible">
+      <i class="close-pop" @click="isDialogVisible= false"></i>
+      <span>生成失败的表情即将全部重新生成？</span>
       <template #footer>
         <div class="footer-btn">
           <span class="btn-cancel" @click="isDialogVisible= false"></span>
-          <span class="btn-ok"  @click="isDialogVisible= false"></span>
+          <span class="btn-ok" @click="toRetryTask()"></span>
         </div>
       </template>
     </nut-dialog>
@@ -52,126 +47,95 @@
 </template>
 
 <script setup>
-import { ref, createVNode } from "vue";
-import { useRouter } from "vue-router";
-import { IconFont,Loading1, MaskClose } from '@nutui/icons-vue'
-import iconLoading from '@/assets/img/icon_loading.png';
-import iconError from '@/assets/img/icon_error.png';
+  import {
+    ref,
+    createVNode
+  } from "vue";
+  import {
+    useRouter,
+    useRoute
+  } from "vue-router";
+  import {
+    IconFont,
+    Loading1,
+    MaskClose
+  } from '@nutui/icons-vue'
+  import iconLoading from '@/assets/img/icon_loading.png';
+  import iconError from '@/assets/img/icon_error.png';
+
+  import {
+    getMyEmoticon,
+    retryTask
+  } from "@/api/api.js";
 
 
-const router = useRouter();
-const previewEmojiData = ref({})
-const emojiList = ref([]);
-const isDialogVisible = ref(false);
+  const router = useRouter();
+  const route = useRoute();
+  console.log("route", route)
+  const previewEmojiData = ref({})
+  const emojiList = ref([]);
+  const isDialogVisible = ref(false);
 
-const handleGenerate = () => {
-  isDialogVisible.value = true
-}
-
-const handlePreview = (item) => {
-  if(item.status == 0) {
-    previewEmojiData.value = item
+  const handleGenerate = (item) => {
+    isDialogVisible.value = true;
   }
-}
 
-// 获取表情包合集数据
-const getCenterList = () => {
+  const handlePreview = (item) => {
+    previewEmojiData.value = item
+    if (item.status == 0) {
+      previewEmojiData.value = item
+    }
+  }
 
-  emojiList.value = [{
-    id: 1,
-    name: '表情包合集0',
-    status: 0,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集1',
-    status: 1,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集2',
-    status: 2,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集3',
-    status: 0,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集4',
-    status: 1,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集5',
-    status: 2,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集3',
-    status: 0,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集4',
-    status: 1,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集5',
-    status: 2,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集3',
-    status: 0,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集4',
-    status: 1,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  },{
-    id: 1,
-    name: '表情包合集5',
-    status: 2,
-    avatar: 'https://img1.baidu.com/it/u=3598104138,3632108415&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800',
-  }]
-  previewEmojiData.value = emojiList.value[0]
-}
-
-function go (item) {
-  router.push({
+  function go(item) {
+    router.push({
       path: `/details`,
       query: {
         id: item.id
       }
     })
-}
-getCenterList()
+  }
+
+  let retryId = "";
+  const getCenterList = async () => {
+    const resp = await getMyEmoticon({
+      mergeId: route.query.mergeId || ""
+    });
+    //status1 生成中 2生成成功 3生成失败 0已删除
+    if (resp.code == 200 && resp.data) {
+      console.log(resp.data)
+      emojiList.value = resp.data.files.filter((item) => item.status != 0);
+      previewEmojiData.value = resp.data.templateInfo
+      retryId = resp.data.taskId || "11-a7049a8b-7d28-49b1-9961-49736ef26e09"
+    }
+  };
+  const toRetryTask = async () => {
+    const resp = await retryTask({
+      taskId: retryId
+    });
+    if (resp.code == 200) {
+      getCenterList();
+    }
+    isDialogVisible.value = false
+  };
 
 
-// onUpdated(getCenterList);
-// onMounted(getCenterList);
-
-
-
-
+  getCenterList()
+  // onUpdated(getCenterList);
+  // onMounted(getCenterList);
 </script>
 
 <style scoped lang="less">
   .list-center-page {
     display: flex;
-    width:100%;
+    width: 100%;
     flex-direction: column;
     padding-top: 0.02rem;
   }
-  
+
   .list {
-    width:100%;
+    width: 100%;
     flex: 1;
     overflow: scroll;
   }
-
 </style>
