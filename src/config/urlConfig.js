bd81bcde1944b3cec55c0656c67d786d0beda91a
liuqiaoyan <961951661@qq.com>
2025-01-10 18:59:12 +0800

    跳转地址

--- src/config/urlConfig.js
+++ src/config/urlConfig.js
@@ -1,6 +1,19 @@
+const url = "https://avatar.migudm.cn/prefe/"
+
+// ---------url
+// production: https://avatar.migudm.cn/h5/
+// pre: https://avatar.migudm.cn/prefe/
+
+
+// --------activityId
+// production: "a1870816073249378306",
+// pre: "a1868848519272112130",
+// stage: "a1868534867359473666",
+// development: "a1867443766334885889",
+
 export default {
     activityId: 'a1868848519272112130',
-    homeUrl: 'https://avatar.migudm.cn/prefe/newyear2025/',
-    centerListUrl: 'https://avatar.migudm.cn/prefe/newyear2025/?page=usercenter&tablv=4_0',
-    payBack: 'https://avatar.migudm.cn/prefe/newyear2025_aigif'
+    homeUrl: url+ "newyear2025/",
+    centerListUrl: url + 'newyear2025/?page=usercenter&tab=aigif',
+    payBack: url + 'newyear2025_aigif'
 }
